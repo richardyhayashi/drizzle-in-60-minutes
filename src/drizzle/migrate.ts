6973bdb698fab790aaff4627441ed3bba3288b00
Richard Y. Hayashi <richardyhayashi@gmail.com>
2025-02-01 20:15:01 +0900

    update

--- src/drizzle/migrate.ts
+++ src/drizzle/migrate.ts
@@ -9,11 +9,15 @@ import postgres from "postgres";
 const migrationClient = postgres(process.env.DATABASE_URL as string, { max: 1});
 
 async function main() {
+   console.log("Migration start...");
+
    await migrate(drizzle(migrationClient), {
       migrationsFolder: "./src/drizzle/migrations"
    });
 
    await migrationClient.end();
+
+   console.log("Migration end...");
 }
 
 main();
